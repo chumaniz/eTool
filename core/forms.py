c23e249e29102cb205cafc20c5fe81bd53145944
chumaniz <131617454+chumaniz@users.noreply.github.com>
2024-01-18 18:33:06 +0200

    Styled sign-up page

--- core/forms.py
+++ core/forms.py
@@ -8,3 +8,24 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields =  ('username', 'email', 'password1', 'password2')
+
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class' :'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'username@email.com',
+        'class' :'w-full py-4 px-6 rounded-xl',
+    }))
+
+    password1 = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Password',
+        'class' :'w-full py-4 px-6 rounded-xl',
+    }))
+
+    password2 = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Same Password',
+        'class' :'w-full py-4 px-6 rounded-xl',
+    }))
